139b8e79110ad33be428bbfa1b505ffdd395ad83
RitchielRez <ritchielrez@gmail.com>
2024-05-09 21:02:19 +0600

    feat: add license information

--- rit_str.h
+++ rit_str.h
@@ -1,3 +1,6 @@
+// LICENSE
+// See end of the file for license information.
+
 #ifndef RIT_STR_INCLUDED
 #define RIT_STR_INCLUDED
 
@@ -185,3 +188,26 @@ void rit_str_view_print(rit_str_view const *t_rit_str_view) {
 
 #endif  // RIT_STR_IMPLEMENTATION
 #endif  // RIT_STR_INCLUDED
+
+/*
+The MIT License (MIT)
+
+Copyright 2024 Ritchiel Reza
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
